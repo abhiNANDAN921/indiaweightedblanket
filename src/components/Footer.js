2dbda82effec5cf06b4205e1ea4d72a3ab9c6449
abhi nandan <abhinandan8891@gmail.com>
2022-05-03 00:00:00 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,16 @@ import React from 'react'
 export default function Footer() {
   return (
     <>
-    <footer class="site-foot"><div class="site-foot-nav container"><div class="site-foot-nav-left"><a aria-current="page" class="" href="/">Weighted Blanket India</a> </div><div class="site-foot-nav-right"><a aria-current="page" class="site-foot-nav-item" href="/">Home</a><a class="site-foot-nav-item" href="/tag/getting-started/">Contact Us</a><a class="site-foot-nav-item" href="/author/ghost/">About Us</a></div></div></footer>
+    <footer class="site-foot">
+      <div class="site-foot-nav container">
+        <div class="site-foot-nav-left">
+          <a aria-current="page" class="" href="/">Weighted Blanket India</a> 
+          </div>
+          <div class="site-foot-nav-right">
+            <a aria-current="page" class="site-foot-nav-item" href="https://weightedblanketindia.com/">Home</a>
+            <a class="site-foot-nav-item" href="https://weightedblanketindia.com/Contact">Contact Us</a>
+          </div>
+            </div></footer>
     </>
   )
 }
